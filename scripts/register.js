127680a09f8f5371ccb195f8de0f402b93129ad3
AshleeHODOR <hodor1987@yahoo.com>
2025-04-11 16:31:19 -0700

    class2homework

--- scripts/register.js
+++ scripts/register.js
@@ -1,47 +1,56 @@
 console.log("register");
 //create an array
-// let pets=["hershey","shadow","nirvana sue"];
-let pets = [];
+let pets = []; //the empty array 
 
-//display name function //loops
-function displayNames(){
-    
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputBreed = document.getElementById("txtBreed");
+let inputService = document.getElementById("txtService");
+
+//object constructor
+function Pet(name, age, gender, breed, service){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.breed = breed;
+    this.service = service;
+}
+
+function register(){
+    //create a new pet
+    let newPet = new Pet();
+    console.log(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value);
+    //push the new pet
+    pets.push(newPet); 
+    //display the name function
+}
+
+//display name function //loop;
+function displayPets(){
+    document.getElementById("results").innerHTML=""; 
     for(let i=0;i<pets.length;i++){
-        console.log(`pet in Pos. ${i} = ${pets[i].name}`);
-
-        document.getElementById("results").innerHTML+=`<li>${pets[i].name}</li>`;//arrayName
+        document.getElementById("results").innerHTML+=`<li>${pets[i].name} - ${pets[i].age}</li>`; 
     }
 }
 
 function init(){
     //create pet objects
-    let pet1 = {
-        name: "Hershey",
-        age: 19,
-        gender: "female", 
-        breed: "husky"
-    }
-    let pet2 = {
-        name: "Shadow",
-        age: 7,
-        gender: "female", 
-        breed: "Domestic short hair"
-    }
-    let pet3 = {
-        name: "Nirvana Sue",
-        age: 2,
-        gender: "male", 
-        breed: "fluffy"
-}
-    //push the pets in the array
+    let pet1 = new Pet("Scooby",80,"Male","dane","no service");
+    let pet2 = new Pet("Scrappy",5,"Male","husky","service dog");
+    let pet3 = new Pet("Hershey",19,"Female","husky","no service");
+
+    //array
     pets.push(pet1,pet2,pet3);
-    displayNames();
     //display names
-   // displayNames("hershey","shadow", "Nirvana Sue");
-
-
+    displayNames("Scooby","Scrappy","Hershey");
 }
 
 window.onload=init; // wait to render the HTML
 
+
+
 //Extra challenge: Create a function to calculate the average of the pet's ages.
+//function averageAge = (number1,number2,number3,number4,number5){
+    //console.log(number +, number / = averageage);
+//}
